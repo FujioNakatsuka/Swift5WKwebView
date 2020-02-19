bd99414b5742bd6652489e8a901613b4ab53050f
Fujio Nakatsuka <com.nifty.cbm01799>
2020-02-19 10:14:14 +0900

    Initial Commit

---
+++ Swift5WKwebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5WKwebView
+//
+//  Created by 中塚富士雄 on 2020/02/19.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
